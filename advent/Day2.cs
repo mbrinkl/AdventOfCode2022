7415e9c4894078bfdb8093c1399e9812295b0927
mbrinkl <bjbrinkleyyy@yahoo.com>
2022-12-08 22:43:36 -0600

    day 2 and 3

---
+++ advent/Day2.cs
@@ -0,0 +1,124 @@
+﻿namespace advent;
+
+public class Day2 : ProblemSet
+{
+    enum Shape
+    {
+        Rock = 1,
+        Paper = 2,
+        Scissors = 3
+    }
+
+    int lossScore;
+    int drawScore;
+    int winScore;
+    Dictionary<string, Shape> shapeDict;
+
+    public Day2(string[] input) : base(input)
+    {
+        shapeDict = new Dictionary<string, Shape>
+        {
+            { "A", Shape.Rock },
+            { "X", Shape.Rock },
+            { "B", Shape.Paper },
+            { "Y", Shape.Paper },
+            { "C", Shape.Scissors },
+            { "Z", Shape.Scissors },
+        };
+
+        lossScore = 0;
+        drawScore = 3;
+        winScore = 6;
+    }
+
+    protected override int P1()
+    {
+        var totalScore = 0;
+
+        foreach (var line in input)
+        {
+            var split = line.Split(" ");
+
+            var opponentShape = shapeDict[split[0]];
+            var myShape = shapeDict[split[1]];
+
+            var outcome = GetOutcome(opponentShape, myShape);
+
+            var score = (int)myShape + outcome;
+
+            totalScore += score;
+        }
+
+        return totalScore;
+    }
+
+
+    protected override int P2()
+    {
+        var totalScore = 0;
+
+        foreach (var line in input)
+        {
+            var split = line.Split(" ");
+
+            var opponentShape = shapeDict[split[0]];
+            var score = GetOutcome(opponentShape, split[1]);
+
+            totalScore += score;
+        }
+
+        return totalScore;
+    }
+
+    private int GetOutcome(Shape s1, Shape s2)
+    {
+        // Draw
+        if (s1 == s2)
+        {
+            return drawScore;
+        }
+
+
+        // Win for s2
+        if (s1 == Shape.Rock && s2 == Shape.Paper ||
+            s1 == Shape.Paper && s2 == Shape.Scissors ||
+            s1 == Shape.Scissors && s2 == Shape.Rock)
+        {
+            return winScore;
+        }
+
+        // Loss for s2
+        return lossScore;
+    }
+
+    private int GetOutcome(Shape s1, string roundEnd)
+    {
+        var score = -1;
+
+        switch(roundEnd)
+        {
+            // Must lose
+            case "X":
+                score = lossScore;
+                if (s1 == Shape.Rock) score += (int)Shape.Scissors;
+                else if (s1 == Shape.Paper) score += (int)Shape.Rock;
+                else if (s1 == Shape.Scissors) score += (int)Shape.Paper;
+                break;
+            // Must draw
+            case "Y":
+                score = drawScore + (int)s1;
+                break;
+            // Must win
+            case "Z":
+                score = winScore;
+                if (s1 == Shape.Rock) score += (int)Shape.Paper;
+                else if (s1 == Shape.Paper) score += (int)Shape.Scissors;
+                else if (s1 == Shape.Scissors) score += (int)Shape.Rock;
+                break;
+            default:
+                break;
+        }
+
+        return score;
+    }
+}
---
+++ advent/Day3.cs
@@ -0,0 +1,55 @@
+﻿namespace advent;
+
+public class Day3 : ProblemSet
+{
+    int a_Value;
+    int A_Value;
+
+    public Day3(string[] input) : base(input)
+    {
+        a_Value = 49;
+        A_Value = 17;
+    }
+
+    protected override int P1()
+    {
+        var totalPriority = 0;
+
+        foreach (var rucksack in input)
+        {
+            var compartment1 = rucksack[..(rucksack.Length / 2)];
+            var compartment2 = rucksack.Substring(rucksack.Length / 2);
+
+            var commonItem = compartment1.Intersect(compartment2).First();
+
+            totalPriority += ComputePriority(commonItem);
+        }
+
+        return totalPriority;
+    }
+
+
+    protected override int P2()
+    {
+        var totalPriority = 0;
+
+        for (int i = 0; i < input.Length; i += 3)
+        {
+            var rucksack1 = input[i];
+            var rucksack2 = input[i + 1];
+            var rucksack3 = input[i + 2];
+
+            var commonItem = rucksack1.Intersect(rucksack2).Intersect(rucksack3).First();
+
+            totalPriority += ComputePriority(commonItem);
+        }
+
+        return totalPriority;
+    }
+
+    private int ComputePriority(char item)
+    {
+        var number = item - '0';
+        return number >= a_Value ? number - a_Value + 1 : number - A_Value + 27;
+    }
+}
